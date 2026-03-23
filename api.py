f8d5c20b6128ccd6ceb5bd4bc49f2d1f545a5f56
Kristina Krauberger <krauberger.kristina@gmail.com>
2026-03-23 12:23:37 +0100

    feat: add PDF loader with DirectoryLoader and PyMuPDF

--- api.py
+++ api.py
@@ -1,12 +1,31 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("SECRET_KEY_OPENAI")
 
 app = Flask(__name__)
+
+loader = DirectoryLoader(
+    path="data/",
+    glob="*.pdf",
+    loader_cls=PyMuPDFLoader
+)
+
+documents = loader.load()
+
+print(f"Anzahl Dokumente: {len(documents)}")
+print("------")
+print(documents[0].page_content[:500])
+print("------")
+print(documents[0].metadata)
+
+
+
 
 @app.route('/')
 def home():
@@ -15,3 +34,5 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5004)
+
+
