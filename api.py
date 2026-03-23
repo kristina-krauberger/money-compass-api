387613041c393341b0e0e5b7e6b4d163e5c9734c
Kristina Krauberger <krauberger.kristina@gmail.com>
2026-03-23 12:30:43 +0100

    feat: add document chunking with RecursiveCharacterTextSplitter

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
@@ -10,6 +11,7 @@ OPENAI_API_KEY = os.getenv("SECRET_KEY_OPENAI")
 
 app = Flask(__name__)
 
+# 1. Load with DirectoryLoader and PyMuPDF
 loader = DirectoryLoader(
     path="data/",
     glob="*.pdf",
@@ -18,13 +20,21 @@ loader = DirectoryLoader(
 
 documents = loader.load()
 
-print(f"Anzahl Dokumente: {len(documents)}")
-print("------")
-print(documents[0].page_content[:500])
-print("------")
-print(documents[0].metadata)
+# print(f"Anzahl Dokumente: {len(documents)}")
+# print("------")
+# print(documents[0].page_content[:500])
+# print("------")
+# print(documents[0].metadata)
 
+# 2. Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = text_splitter.split_documents(documents)
 
+print(f"Anzahl Chunks: {len(chunks)}")
+print("------")
+print(chunks[0].page_content)
+print("------")
+print(chunks[0].metadata)
 
 
 @app.route('/')
